337e51248395f7af4220b428de8134c016034230
Simon Errington <simon@maprunner.co.uk>
2013-12-09 13:44:54 +0000

    Check GPS file is on the map

--- js/rg2.js
+++ js/rg2.js
@@ -95,68 +95,33 @@ jQuery(document).ready(function() {
 		  var AEDB = (w.A * w.E) - (w.D * w.B);
 		  var xCorrection = (w.B * w.F) - (w.E * w.C);
 		  var yCorrection = (w.D * w.C) - (w.A * w.F);
+		  var i;
 		  for (i = 0; i < this.lat.length; i++) {
 			  draw.routeData.x[i] = Math.round(((w.E * this.lon[i]) - (w.B * this.lat[i]) + xCorrection) / AEDB);
 		    draw.routeData.y[i] = Math.round(((-1 * w.D * this.lon[i]) + (w.A * this.lat[i]) + yCorrection) / AEDB);
 		  }		
-		} else {
-      // fit track to within limits of course
-      
 		  // find bounding box for track
-		  var maxLat = this.lat[0];
-		  var maxLon = this.lon[0];
-		  var minLat = this.lat[0];
-		  var minLon = this.lon[0] ;
-		  for (var i = 1; i < this.lat.length; i++) {
-		  	maxLat = Math.max(maxLat, this.lat[i]);
-		  	maxLon = Math.max(maxLon, this.lon[i]);
-		  	minLat = Math.min(minLat, this.lat[i]);
-		  	minLon = Math.min(minLon, this.lon[i]);
-		    x = this.lon[i] - this.lon[0];
-		    y = this.lat[i] - this.lat[0];
+		  var minX = draw.routeData.x[0];
+		  var maxX = draw.routeData.x[0];
+		  var minY = draw.routeData.y[0];
+		  var maxY = draw.routeData.y[0];
+		  
+		  for (i = 1; i < draw.routeData.x.length; i++) {
+			  maxX = Math.max(maxX, draw.routeData.x[i]);
+			  maxY = Math.max(maxY, draw.routeData.y[i]);
+		 	  minX = Math.min(minX, draw.routeData.x[i]);
+		 	  minY = Math.min(minY, draw.routeData.y[i]);
 		  }
+		  // check we are somewhere on the map
+      if ((maxX < 0) || (minX > map.width) || (minY > map.height) || (maxY < 0)) {
+        // warn and fit to track      	
+        var msg = "<div id='GPS-problem-dialog'>Your GPS file does not match the map co-ordinates. Please check you have selected the correct file.</div>";
+        jQuery(msg).dialog({title:"GPS file problem"});
+        this.fitTrackInsideCourse();
 
-		  // find bounding box for course
-		  var minControlX = draw.controlx[0];
-		  var maxControlX = draw.controlx[0];
-		  var minControlY = draw.controly[0];
-		  var maxControlY = draw.controly[0];
-		  
-		  for (i = 0; i < draw.controlx.length; i++) {
-		  	maxControlX = Math.max(maxControlX, draw.controlx[i]);
-		  	maxControlY = Math.max(maxControlY, draw.controly[i]);
-		  	minControlX = Math.min(minControlX, draw.controlx[i]);
-		  	minControlY = Math.min(minControlY, draw.controly[i]);
-		  }
-      //console.log (minControlX, maxControlX, minControlY, maxControlY);
-
-      // scale GPS track to within bounding box of controls: a reasonable start
-		  var scaleX = (maxControlX - minControlX)/(maxLon - minLon);
-		  var scaleY = (maxControlY - minControlY)/(maxLat - minLat);
-      var lonCorrection = getLatLonDistance(minLat, maxLon, minLat, minLon) / (maxLon - minLon);
-		  var latCorrection = getLatLonDistance(minLat, minLon, maxLat, minLon) / (maxLat - minLat);
-		  
-		  // don't want to skew track so scale needs to be equal in each direction
-		  // so we need to account for differences between a degree of latitude and longitude	  
-		  if (scaleX > scaleY) {
-		  	// pix/lat = pix/lon * m/lat * lon/m
-		  	scaleY = scaleX * latCorrection / lonCorrection;
-		  } else {
-		  	// pix/lon = pix/lat * m/lon * lat/m
-		  	scaleX = scaleY * lonCorrection / latCorrection;  	
-		  }
-      // extra offset to put start of track at start location 
-		  draw.routeData.x[0] = ((this.lon[0] - minLon) * scaleX) + minControlX;
-		  draw.routeData.y[0] = (-1 * (this.lat[0] - maxLat) * scaleY) + minControlY;
-		  
-		  // translate lat/lon to x,y
-		  var deltaX = minControlX - (draw.routeData.x[0] - draw.controlx[0]); 
-		  var deltaY = minControlY - (draw.routeData.y[0] - draw.controly[0]);
-	  
-		  for (i = 0; i < this.lat.length; i++) {
-		  	draw.routeData.x[i] = ((this.lon[i] - minLon) * scaleX) + deltaX;
-		  	draw.routeData.y[i] = (-1 * (this.lat[i] - maxLat) * scaleY) + deltaY;
       }
+		} else {
+      this.fitTrackInsideCourse();
     }
 		this.baseX = draw.routeData.x.slice(0);
 		this.baseY = draw.routeData.y.slice(0);
@@ -164,9 +129,71 @@ jQuery(document).ready(function() {
     this.fileLoaded = true;
     jQuery("#btn-save-gps-route").button("enable");
 		redraw(false);
-    }
-  }
+  }, 
+  
+  fitTrackInsideCourse: function() {
+    // fit track to within limits of course
+		// find bounding box for track
+		var maxLat = this.lat[0];
+		var maxLon = this.lon[0];
+		var minLat = this.lat[0];
+		var minLon = this.lon[0];
+		var x;
+		var y;
+		for (var i = 1; i < this.lat.length; i++) {
+		 	maxLat = Math.max(maxLat, this.lat[i]);
+			maxLon = Math.max(maxLon, this.lon[i]);
+			minLat = Math.min(minLat, this.lat[i]);
+			minLon = Math.min(minLon, this.lon[i]);
+		  x = this.lon[i] - this.lon[0];
+		  y = this.lat[i] - this.lat[0];
+		}
 
+		// find bounding box for course
+		var minControlX = draw.controlx[0];
+		var maxControlX = draw.controlx[0];
+		var minControlY = draw.controly[0];
+		var maxControlY = draw.controly[0];
+		  
+		for (i = 1; i < draw.controlx.length; i++) {
+			maxControlX = Math.max(maxControlX, draw.controlx[i]);
+			maxControlY = Math.max(maxControlY, draw.controly[i]);
+		 	minControlX = Math.min(minControlX, draw.controlx[i]);
+		 	minControlY = Math.min(minControlY, draw.controly[i]);
+		}
+    //console.log (minControlX, maxControlX, minControlY, maxControlY);
+
+    // scale GPS track to within bounding box of controls: a reasonable start
+		var scaleX = (maxControlX - minControlX)/(maxLon - minLon);
+		var scaleY = (maxControlY - minControlY)/(maxLat - minLat);
+    var lonCorrection = getLatLonDistance(minLat, maxLon, minLat, minLon) / (maxLon - minLon);
+		var latCorrection = getLatLonDistance(minLat, minLon, maxLat, minLon) / (maxLat - minLat);
+		  
+		// don't want to skew track so scale needs to be equal in each direction
+		// so we need to account for differences between a degree of latitude and longitude	  
+		if (scaleX > scaleY) {
+			// pix/lat = pix/lon * m/lat * lon/m
+			scaleY = scaleX * latCorrection / lonCorrection;
+		} else {
+			// pix/lon = pix/lat * m/lon * lat/m
+			scaleX = scaleY * lonCorrection / latCorrection;  	
+		}
+    // extra offset to put start of track at start location 
+		draw.routeData.x[0] = ((this.lon[0] - minLon) * scaleX) + minControlX;
+		draw.routeData.y[0] = (-1 * (this.lat[0] - maxLat) * scaleY) + minControlY;
+		  
+		// translate lat/lon to x,y
+		var deltaX = minControlX - (draw.routeData.x[0] - draw.controlx[0]); 
+		var deltaY = minControlY - (draw.routeData.y[0] - draw.controly[0]);
+	  
+		for (i = 0; i < this.lat.length; i++) {
+			draw.routeData.x[i] = ((this.lon[i] - minLon) * scaleX) + deltaX;
+			draw.routeData.y[i] = (-1 * (this.lat[i] - maxLat) * scaleY) + deltaY;
+    }
+    
+  },
+}
+  
   // handle drawing of a new route
   function Draw() {
     this.trackColor = '#ff0000';
