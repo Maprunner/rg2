ea2ac808df20b2d0770a936945872962a4aa5615
Simon Errington <simon@maprunner.co.uk>
2014-02-17 13:04:12 +0000

    Update version of jQuery to 1.11.0

--- index.php
+++ index.php
@@ -283,7 +283,7 @@
       <?php } ?>
     </div>
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     <script type="text/javascript">
       var json_url = "<?php echo $json_url; ?>";
