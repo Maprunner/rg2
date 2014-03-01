f088b24ffe3d102ea581a46ae2f6ef0b0dff24f3
Simon Errington <simon@maprunner.co.uk>
2014-03-01 16:05:58 +0000

    Refactor index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $maps_url = RG_BASE_DIRECTORY . "/kartat/";
 
 // include manager function as parameter for now until we decide the best way forward
 if (isset($_GET['manage'])) {
-	$manager = true;
+	$manager = TRUE;
   if (defined('OVERRIDE_KARTAT_DIRECTORY')) {
     $manager_url = OVERRIDE_KARTAT_DIRECTORY;
   } else {
@@ -51,14 +51,14 @@ if (isset($_GET['manage'])) {
   $keksi = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"), 0, 20);
   file_put_contents($manager_url."keksi.txt", $keksi.PHP_EOL); 
 } else {
-	$manager = false;
+	$manager=  FALSE;
 }
 
 // include debug function as parameter for now until we decide the best way forward
 if (isset($_GET['debug']) || $override) {
-	$debug = true;
+	$debug = TRUE;
 } else {
-	$debug = false;
+	$debug = FALSE;
 }
 
 ?>
@@ -81,27 +81,12 @@ if (isset($_GET['debug']) || $override) {
     <link rel="stylesheet" href="css/rg2.css">
     <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/<?php echo $ui_theme; ?>/jquery-ui.min.css">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
- 
   </head>
   <body>
     <!--[if lt IE 7]>
     <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
-
-    <div id="rg2-header-container">
-      <div  id="rg2-resize-info" title="Hide info panel">
-        <a href="#"><img id="rg2-resize-info-icon" class="hide" src='<?php echo $img_url . "rg2-logo-32x32.png"; ?>'></a>
-      </div>
-      <div id="rg2-header"><span id="rg2-event-title">Routegadget 2.0</span></div>  
-        <div class="rg2-button"><i id="btn-about" title = "Help" class="fa fa-question"></i></div>
-        <div class="rg2-button"><i id="btn-options" title = "Options" class="fa fa-cog"></i></div>
-        <div class="rg2-button"><i id="btn-zoom-out" title = "Zoom out" class="fa fa-search-minus"></i></div>
-        <div class="rg2-button"><i id="btn-reset" title = "Reset" class="fa fa-undo"></i></div>
-        <div class="rg2-button"><i id="btn-zoom-in" title = "Zoom in" class="fa fa-search-plus"></i></div>
-        <div class="rg2-button"><i id="btn-show-splits" title = "Splits" class="fa fa-list-alt"></i></div>
-        <div class="rg2-button"><i  id="btn-toggle-controls" title = "Show controls" class="fa fa-circle-o"></i></div>
-        <div class="rg2-button"><i id="btn-toggle-names" title = "Show initials" class="fa fa-tag"></i></div> 
-    </div>
+    <?php include 'html/header.html'; ?>
     <div id="rg2-container">
       <div id="rg2-info-panel">
         <div id="rg2-info-panel-tab-headers">
@@ -170,81 +155,22 @@ if (isset($_GET['debug']) || $override) {
        <button id="btn-undo-gps-adjust">Undo</button>
        <button class="pushright" id="btn-save-gps-route">Save GPS route</button> 
     </div>
-    <?php if ($manager) {include 'manager.html'; } ?>     
+    <?php if ($manager) {include 'html/manager.html'; } ?>     
        </div>
       </div>
-      <canvas id="rg2-map-canvas">Your browser does not support HTML5</canvas>
-      <div id="rg2-animation-controls">
-        <div class="rg2-ani-row row-1">
-          <div class="rg2-button"><i id="btn-slower" title = "Slower" class="fa fa-minus"></i></div>
-          <div class="rg2-button"><i id="btn-start-stop" title = "Run" class="fa fa-play"></i></div>
-          <div class="rg2-button"><i id="btn-faster" title = "Faster" class="fa fa-plus"></i></div>
-          <div id="rg2-clock"></div>
-        </div>
-        <div class="rg2-ani-row row-2">
-          <div id="rg2-clock-slider"></div>
-        </div>
-        <div class="rg2-ani-row row-3">
-          <div id="rg2-replay-start-control">
-            Start at: <select  id="rg2-control-select"><option>S</option></select>
-          </div>
-          <div class="rg2-button"><i id="btn-real-time" title = "Real time" class="fa fa-clock-o"></i></div>
-          <div class="rg2-button"><i id="btn-mass-start" title = "Mass start" class="fa fa-users"></i></div>
-        </div>
-        <div class="rg2-ani-row row-4">
-          <div id="rg2-tails-spinner">
-            <label for="spn-tail-length">Length</label>
-            <input id="spn-tail-length" name="value" />
-          </div>
-          <div id="rg2-tails-type">
-		        <label for="btn-full-tails">Full tails </label>
-		        <input type="checkbox" id="btn-full-tails" />
-		      </div>
-		    </div>
-		  </div>
-		  <div id="rg2-hide-info-panel-control">
-		    <a href="#"><i id="rg2-hide-info-panel-icon" class="fa fa-chevron-left"></i></a>
-		  </div>		   
-		  <div id="rg2-option-controls" title="Configuration options">
-        <div id="rg2-dim-spinner">
-          <label for="spn-map-intensity">Map intensity %</label>
-          <input id="spn-map-intensity" name="value" />
-        </div>
-        <div id="rg2-course-width-spinner">
-          <label for="spn-course-width">Course overprint width</label>
-          <input id="spn-course-width" name="value" />
-        </div>
-        <div id="rg2-route-width-spinner">
-          <label for="spn-route-width">Route width</label>
-          <input id="spn-route-width" name="value" />
-        </div>
-        <div id="rg2-show-seconds">
-          <label for="chk-show-three-seconds"> Show +3 time loss for GPS routes </label><input type=checkbox id="chk-show-three-seconds">
-        </div>
-      </div>
-		  <div id="rg2-track-names"></div>
-		  <div id="rg2-about-dialog" title="Routegadget 2.0">
-			 <p>This application allows you to view existing <a href="http://www.routegadget.net">Routegadget</a> information in any modern (HTML5-compliant) browser without the need for Java.</p>
-			 <p>The latest version is available for
-			 	<a href="https://github.com/Maprunner/rg2/archive/master.zip"> download here</a>.
-				 Later versions will allow you to create new events, but for now it only works on events that have been set
-			   up in in the original Routegadget.</p>
-			  <p><strong>Simon Errington</strong> (simon (at) maprunner.co.uk)</p>
-        <p id="rg2-version-info"></p>
-        <p><?php echo ADDITIONAL_INFO_TEXT; ?></p>
-      </div>
-      <div id="rg2-splits-table" title="Splits display"></div>
+      <?php include 'html/animation.html'; ?>   
+      <?php include 'html/options.html'; ?>
+      <?php include 'html/misc.html'; ?>
     </div>
-
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
     <script type="text/javascript">var json_url =  "<?php echo $json_url; ?>";
       var maps_url = "<?php echo $maps_url; ?>";
       var header_colour = "<?php echo $header_colour; ?>";
       var header_text_colour = "<?php echo $header_text_colour; ?>";
-      <?php if ($manager) { ?>
+    <?php if ($manager) { ?>
       var keksi = "<?php echo $keksi; ?>";
-      <?php } ?>
+    <?php } ?>
     </script>
     <?php if ($debug) { ?>
       <script src='<?php echo $script_url . "events.js"; ?>'></script>
@@ -260,8 +186,8 @@ if (isset($_GET['debug']) || $override) {
     <?php } else { ?>
       <script src='<?php echo $script_url . "rg2all.min.js"; ?>'></script>      
     <?php } ?>  
-      <?php if ($manager) { ?>
+    <?php if ($manager) { ?>
       <script src='<?php echo $script_url . "manager.js"; ?>'></script>
-      <?php } ?>
+    <?php } ?>
   </body>
 </html>
