06ef9362c8a71ca1b5d391c4c4d69d1a6acc9495
Maprunner <simon@maprunner.co.uk>
2016-02-01 19:40:12 +0000

    Version 1.2.4
    Closes #298

--- rg2api.php
+++ rg2api.php
@@ -30,7 +30,7 @@
   }
 
   // version replaced by Gruntfile as part of release 
-  define ('RG2VERSION', '1.2.3');
+  define ('RG2VERSION', '1.2.4');
   define ('KARTAT_DIRECTORY', $url);
   define ('LOCK_DIRECTORY', dirname(__FILE__)."/lock/saving/");
   define ('CACHE_DIRECTORY', $url."cache/");
