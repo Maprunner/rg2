3519557e61de2334fc42d75449b7724ca8028bc9
Maprunner <simon@maprunner.co.uk>
2017-02-02 15:03:46 +0000

    Version 1.3.3

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // version replaced by Gruntfile as part of release
-define ('RG2VERSION', '1.3.2');
+define ('RG2VERSION', '1.3.3');
 
 if (file_exists( dirname(__FILE__) . '/rg2-config.php')) {
   require_once( dirname(__FILE__) . '/rg2-config.php' );
