c2ac27dc7b4594564b9a8fb938f026dd5fa3b598
James Errington <jmerrington07@gmail.com>
2017-06-18 20:33:35 +0100

    Add Meta tags for share testing

--- index.php
+++ index.php
@@ -97,6 +97,10 @@ header('Content-type: text/html; charset=utf-8');
     <title>Routegadget 2</title>
     <meta name="description" content="View and save route choices for orienteering events">
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@MaprunnerGB" />
+    <meta property="og:title" content="Title" />
+    <meta property="og:description" content="Description" />
     <!-- favicon info -->
     <link rel="apple-touch-icon" sizes="180x180" href='<?php echo $source_url ."/img/apple-touch-icon.png"; ?>'>
     <link rel="icon" type="image/png" href='<?php echo $source_url ."/img/favicon-32x32.png"; ?>' sizes="32x32">
