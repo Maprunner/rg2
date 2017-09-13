5a2e1540c66e98ba468b1f853a23dbffb3bbb4ab
Maprunner <simon@maprunner.co.uk>
2017-09-13 12:50:59 +0100

    PageSpeed optimizaions

--- index.php
+++ index.php
@@ -264,7 +264,7 @@ header('Content-type: text/html; charset=utf-8');
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
+  js.src = "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.9";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
