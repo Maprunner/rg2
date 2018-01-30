da863601d60c2f33f963dd3441922cf7959c7ba1
Mikko K <mki@iki.fi>
2018-01-31 01:05:53 +0200

    Aling lines.

--- js/resultparseriofv3.js
+++ js/resultparseriofv3.js
@@ -72,11 +72,10 @@
       if (time.length > 0 && time[0].textContent) {
         var timeInt = parseInt(time[0].textContent, 10);
         if (timeInt <= 3600) {
-            return rg2.utils.formatSecsAsMMSS(timeInt);
+          return rg2.utils.formatSecsAsMMSS(timeInt);
         } else {
-            return rg2.utils.formatSecsAsHHMMSS(timeInt);
+          return rg2.utils.formatSecsAsHHMMSS(timeInt);
         }
-        
       }
       return '00:00';
     },
