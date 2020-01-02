fb2302a42f131f5c62ddbc8b56ef9c9a0fd0a65c
Maprunner <simon@maprunner.co.uk>
2020-01-02 16:54:28 +0000

    Enhanced statistics
    Closes #456

--- js/stats.js
+++ js/stats.js
@@ -9,6 +9,8 @@
     this.byLegPos = [];
     this.byRacePos = [];
     this.resultIndex = null;
+    // empirical setting for how often to iterate loss assessment
+    this.iterations = 3;
   }
 
   Stats.prototype = {
@@ -69,6 +71,7 @@
         active: 0
       });
       $("#rg2-stats-table").dialog({
+        title: this.result.name,
         resizable: false,
         maxHeight: $("#rg2-map-canvas").height() * 0.98,
         width: Math.min($("#rg2-map-canvas").width() * 0.8, 1000),
@@ -77,7 +80,7 @@
     },
 
     generateSummary: function () {
-      var html, info;
+      var html, info, ratios;
       info = this.getLegPosInfo();
       html = rg2.t('Name') + ': <strong>' + this.result.name + '</strong><br>' + rg2.t('Course') + ':<strong>' + this.result.coursename + '</strong><br>';
       html += rg2.t('Time') + ': <strong>' + this.result.time + '</strong><br>';
@@ -88,8 +91,26 @@
       if (this.isNumberOverZero(this.result.timeInSecs)) {
         html += ' (' + (100 * this.results[this.resultIndex].totalLoss / this.result.timeInSecs).toFixed(1) + ' %)';
       }
-      html += '</strong>';
+      html += '</strong><br>';
+      html += rg2.t('Performance') + ': <strong>' + (this.result.performanceIndex[this.iterations - 1] * 100).toFixed(1) + '%</strong><br>';
+      // first entry is 0 for start control, so throw that away    
+      ratios = this.results[this.resultIndex].refRatio.slice(1);
+      html += rg2.t('Consistency') + ': <strong>' + (100 * this.getStandardDeviation(ratios)).toFixed(1) + '%</strong>';
       $("#rg2-stats-summary").empty().append(html);
+    },
+
+    getMean: function (data) {
+      return data.reduce(function (a, b) {
+        return a + b;
+      }, 0) / data.length;
+    },
+
+    getStandardDeviation: function (values) {
+      var mean;
+      mean = this.getMean(values);
+      return Math.sqrt(values.reduce(function (sq, n) {
+        return sq + Math.pow(n - mean, 2);
+      }, 0) / (values.length));
     },
 
     isNumberOverZero: function (n) {
@@ -120,7 +141,7 @@
           }
         }
         if (i === 0) {
-          row.time = '0.00';
+          row.time = '0:00';
         } else {
           row.time = rg2.utils.formatSecsAsMMSS(this.results[this.resultIndex].legSplits[i]);
         }
@@ -128,6 +149,11 @@
           row.position = '-';
         } else {
           row.position = this.results[this.resultIndex].legpos[i];
+        }
+        if (i === 0) {
+          row.performance = '-';
+        } else {
+          row.performance = (100 * this.results[this.resultIndex].refRatio[i]).toFixed(1);
         }
         row.best = rg2.utils.formatSecsAsMMSS(this.byLegPos[i][0].t);
         if (i === 0) {
@@ -162,7 +188,7 @@
             row.percent = parseInt((behind * 100 / this.byLegPos[i][0].t), 10);
           }
         }
-        row.predicted = rg2.utils.formatSecsAsMMSS(this.results[this.resultIndex].predictedSplit[i]);
+        row.predicted = rg2.utils.formatSecsAsMMSS(this.results[this.resultIndex].predictedSplits[i]);
         if (this.results[this.resultIndex].legSplits[i] === 0) {
           row.loss = '-';
         } else {
@@ -173,22 +199,53 @@
 
       var gridOptions = {
         columnDefs: [
-          { headerName: rg2.t("Control"), field: "control", headerClass: "align-center", cellClass: "align-center", width: 88 },
-          { headerName: rg2.t("Time"), field: "time", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: rg2.t("Position"), field: "position", headerClass: "align-center", cellClass: "align-center", width: 100 },
-          { headerName: rg2.t("Best"), field: "best", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: rg2.t("Who"), field: "who", width: 200, tooltipField: "who" },
-          { headerName: rg2.t("Behind"), field: "behind", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: "%", field: "percent", headerClass: "align-center", cellClass: "align-center", width: 60 },
-          { headerName: rg2.t("Predicted"), field: "predicted", headerClass: "align-center", cellClass: "align-center", width: 110 },
-          { headerName: rg2.t("Loss"), field: "loss", headerClass: "align-center", cellClass: "align-center", width: 85 }
+          { headerName: rg2.t("Control"), field: "control", width: 80 },
+          { headerName: rg2.t("Time"), field: "time", width: 80, comparator: this.timeComparator.bind(this) },
+          { headerName: rg2.t("Position"), field: "position", width: 75 },
+          { headerName: rg2.t("Performance"), field: "performance", width: 95, comparator: this.perfComparator.bind(this) },
+          { headerName: rg2.t("Best"), field: "best", width: 80 },
+          { headerName: rg2.t("Who"), field: "who", headerClass: "align-left", cellClass: "align-left", width: 180, tooltipField: "who" },
+          { headerName: rg2.t("Behind"), field: "behind", width: 80, comparator: this.timeComparator.bind(this) },
+          { headerName: "%", field: "percent", width: 60 },
+          { headerName: rg2.t("Predicted"), field: "predicted", width: 100, comparator: this.timeComparator.bind(this) },
+          { headerName: rg2.t("Loss"), field: "loss", width: 75, comparator: this.timeComparator.bind(this) }
         ],
         rowData: rowData,
         domLayout: 'autoHeight',
+        defaultColDef: {
+          headerClass: "align-center",
+          cellClass: "align-center",
+          sortable: true
+        }
       };
 
       $('#rg2-leg-table').empty();
       new agGrid.Grid(document.querySelector('#rg2-leg-table'), gridOptions);
+    },
+
+    timeComparator: function (t1, t2) {
+      return (this.getTimeValue(t1) - this.getTimeValue(t2));
+    },
+
+    getTimeValue: function (t) {
+      // gets "-" or "mm:ss"
+      if (t === "-") {
+        return 0;
+      } else {
+        return parseFloat(t.replace(":", "."));
+      }
+    },
+
+    perfComparator: function (p1, p2) {
+      return (this.getPerfValue(p1) - this.getPerfValue(p2));
+    },
+
+    getPerfValue: function (p) {
+      if (p === "-") {
+        return 0;
+      } else {
+        return parseFloat(p);
+      }
     },
 
     getTimeFromLegPos: function (legpos) {
@@ -197,6 +254,9 @@
 
     getAverages: function (data, perCent) {
       var i, total, count, adjustedCount, median;
+      if (data.length === 0) {
+        return ({ mean: 0, median: 0 });
+      }
       // sort incoming values
       data.sort(function compare(a, b) {
         return a - b;
@@ -215,9 +275,6 @@
 
       for (i = 0; i < count; i += 1) {
         total = total + data[i];
-      }
-      if (count === 0) {
-        return ({ mean: 0, median: 0 });
       }
       if (count === 1) {
         median = data[0];
@@ -248,7 +305,7 @@
           }
         }
         if (i == 0) {
-          row.time = '0.00';
+          row.time = '0:00';
         } else {
           if (this.results[this.resultIndex].splits[i] === this.results[this.resultIndex].splits[i - 1]) {
             row.time = "";
@@ -293,17 +350,21 @@
 
       var gridOptions = {
         columnDefs: [
-          { headerName: rg2.t("Control"), field: "control", headerClass: "align-center", cellClass: "align-center", width: 88 },
-          { headerName: rg2.t("Time"), field: "time", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: rg2.t("Position"), field: "position", headerClass: "align-center", cellClass: "align-center", width: 100 },
-          { headerName: rg2.t("Best"), field: "best", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: rg2.t("Who"), field: "who", width: 200, tooltipField: "who" },
-          { headerName: rg2.t("Behind"), field: "behind", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: "%", field: "percent", headerClass: "align-center", cellClass: "align-center", width: 85 },
-          { headerName: "Loss", field: "loss", headerClass: "align-center", cellClass: "align-center", width: 100 }
+          { headerName: rg2.t("Control"), field: "control", width: 88 },
+          { headerName: rg2.t("Time"), field: "time", width: 85 },
+          { headerName: rg2.t("Position"), field: "position", width: 100 },
+          { headerName: rg2.t("Best"), field: "best", width: 85 },
+          { headerName: rg2.t("Who"), field: "who", headerClass: "align-left", cellClass: "align-left", width: 200, tooltipField: "who" },
+          { headerName: rg2.t("Behind"), field: "behind", width: 85 },
+          { headerName: "%", field: "percent", width: 85 },
+          { headerName: "Loss", field: "loss", width: 100 }
         ],
         rowData: rowData,
-        domLayout: 'autoHeight'
+        domLayout: 'autoHeight',
+        defaultColDef: {
+          headerClass: "align-center",
+          cellClass: "align-center"
+        }
       };
 
       $('#rg2-race-table').empty();
@@ -311,17 +372,19 @@
     },
 
     generateSplitsTable: function () {
-      var i, j, r, height, row, rowData, columnDefs;
+      var i, j, r, height, row, rowData, columnDefs, ratios;
       columnDefs = [
-        { headerName: rg2.t("Pos"), field: "position", headerClass: "align-center", cellClass: "align-center", width: 60, pinned: "left", sortable: true },
-        { headerName: rg2.t("Name"), field: "name", width: 150, pinned: "left" },
-        { headerName: rg2.t("Time"), field: "time", headerClass: "align-center", cellClass: "align-center", width: 85 },
+        { headerName: rg2.t("Pos"), field: "position", width: 60, pinned: "left", sortable: true },
+        { headerName: rg2.t("Name"), field: "name", headerClass: "align-left", cellClass: "align-left", width: 150, pinned: "left" },
+        { headerName: rg2.t("Time"), field: "time", width: 85 },
       ];
       for (j = 1; j < this.controls - 1; j += 1) {
-        columnDefs.push({ headerName: j, field: 'C' + j, cellRenderer: this.renderSplits, headerClass: "align-center", cellClass: "align-center", width: 110 });
+        columnDefs.push({ headerName: j, field: 'C' + j, cellRenderer: this.renderSplits, width: 110 });
       }
-      columnDefs.push({ headerName: rg2.t('F'), field: 'finish', cellRenderer: this.renderSplits, headerClass: "align-center", cellClass: "align-center", width: 110 });
-      columnDefs.push({ headerName: rg2.t('Loss'), field: 'loss', headerClass: "align-center", cellClass: "align-center", width: 100 });
+      columnDefs.push({ headerName: rg2.t('F'), field: 'finish', cellRenderer: this.renderSplits, width: 110 });
+      columnDefs.push({ headerName: rg2.t('Loss'), field: 'loss', width: 100 });
+      columnDefs.push({ headerName: rg2.t('Performance'), field: 'performance', width: 100 });
+      columnDefs.push({ headerName: rg2.t('Consistency'), field: 'consistency', width: 100 });
 
       rowData = [];
       // sort results table: this gets round problems of having multiple classes on one course where results were by class
@@ -358,7 +421,10 @@
         }
         row.finish = { split: rg2.utils.formatSecsAsMMSS(r.splits[this.controls - 1]), pos: r.racepos[this.controls - 1] };
         row.loss = rg2.utils.formatSecsAsMMSS(r.timeInSecs - r.totalLoss);
-        row.initials = r.initials;
+        row.performance = (r.performanceIndex[this.iterations - 1] * 100).toFixed(1);
+        // first entry is 0 for start control, so throw that away    
+        ratios = r.refRatio.slice(1);
+        row.consistency = (100 * this.getStandardDeviation(ratios)).toFixed(1);
         rowData.push(row);
         row = {};
         for (j = 1; j < this.controls - 1; j += 1) {
@@ -371,7 +437,11 @@
 
       var gridOptions = {
         columnDefs: columnDefs,
-        rowData: rowData
+        rowData: rowData,
+        defaultColDef: {
+          headerClass: "align-center",
+          cellClass: "align-center"
+        }
       };
 
       // can't get ag-grid examples to work in terms of height adjustment so this is
@@ -475,45 +545,81 @@
     },
 
     calculateLostTime: function () {
-      var i, k, averages, times, ratios, loss;
-      // find reference times for each leg
+      var i, k, iter, splits;
       this.course.refLegTime = [];
+      for (i = 0; i < this.results.length; i += 1) {
+        this.results[i].performanceIndex = [];
+        this.results[i].iterSplits = [];
+        splits = [];
+        for (k = 0; k < this.controls; k += 1) {
+          splits.push(this.results[i].legSplits[k]);
+        }
+        this.results[i].iterSplits.push(splits.slice(0));
+      }
+
+      for (iter = 0; iter < this.iterations; iter += 1) {
+        this.iterateLostTime(iter);
+        for (i = 0; i < this.results.length; i += 1) {
+          splits.length = 0;
+          for (k = 0; k < this.controls; k += 1) {
+            splits.push(Math.min(this.results[i].predictedSplits[k], this.results[i].legSplits[k]));
+          }
+          this.results[i].iterSplits.push(splits.slice(0));
+        }
+      }
+    },
+
+    iterateLostTime: function (iter) {
+
+      var i, k, averages, times, ratios, loss, refTimes;
+      // find reference times for each leg
       times = [];
+      refTimes = [];
       for (i = 0; i < this.controls; i += 1) {
         times.length = 0;
         for (k = 0; k < this.results.length; k += 1) {
-          if (this.results[k].legSplits[i] !== 0) {
-            times.push(this.results[k].legSplits[i]);
+          if (this.results[k].iterSplits[iter][i] !== 0) {
+            times.push(this.results[k].iterSplits[iter][i]);
           }
         }
         // using average of best 25% of times for the leg
         averages = this.getAverages(times, 25);
-        this.course.refLegTime[i] = averages.median;
+        refTimes.push(averages.median);
       }
+      this.course.refLegTime.push(refTimes);
+
       // find ratio to reference times
       ratios = [];
       for (i = 0; i < this.results.length; i += 1) {
         this.results[i].refRatio = [];
         this.results[i].refRatio[0] = 0;
         ratios.length = 0;
-        for (k = 1; k < this.controls; k += 1) {
-          this.results[i].refRatio[k] = this.results[i].legSplits[k] / this.course.refLegTime[k];
+        for (k = 0; k < this.controls; k += 1) {
+          if (this.results[i].iterSplits[iter][k] === 0) {
+            this.results[i].refRatio[k] = 0;
+          } else {
+            this.results[i].refRatio[k] = this.course.refLegTime[iter][k] / this.results[i].iterSplits[iter][k];
+          }
           ratios.push(this.results[i].refRatio[k]);
         }
         averages = this.getAverages(ratios, 100);
-        this.results[i].medianRefRatio = averages.median;
+        this.results[i].performanceIndex.push(averages.median);
       }
 
       // find predicted times and losses
       for (i = 0; i < this.results.length; i += 1) {
-        this.results[i].predictedSplit = [];
-        this.results[i].predictedSplit[0] = 0;
+        this.results[i].predictedSplits = [];
+        this.results[i].predictedSplits[0] = 0;
         this.results[i].loss = [];
         this.results[i].loss[0] = 0;
         loss = 0;
-        for (k = 1; k < this.controls; k += 1) {
-          this.results[i].predictedSplit[k] = parseInt(this.results[i].medianRefRatio * this.course.refLegTime[k], 10);
-          this.results[i].loss[k] = this.results[i].legSplits[k] - this.results[i].predictedSplit[k];
+        for (k = 0; k < this.controls; k += 1) {
+          if (this.results[i].performanceIndex[iter] > 0) {
+            this.results[i].predictedSplits[k] = parseInt(this.course.refLegTime[iter][k] / this.results[i].performanceIndex[iter], 10);
+          } else {
+            this.results[i].predictedSplits[k] = this.results[i].legSplits[k];
+          }
+          this.results[i].loss[k] = this.results[i].legSplits[k] - this.results[i].predictedSplits[k];
           if (this.results[i].loss[k] < 0) {
             this.results[i].loss[k] = 0;
           }
