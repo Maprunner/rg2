1370781e5eeb18b6d4aef39e90fb4e1dfb5e31f0
James Errington <james.errington@protonmail.com>
2021-12-21 22:54:54 +0000

    Add StyleLint and update Gruntfile

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+module.exports = {
+  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  ignoreFiles: ["**/*.min.css"],
+  rules: {
+    
+  }
+};
