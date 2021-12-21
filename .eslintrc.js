d5898577499a71cf16f45b949f1587e1263bb617
James Errington <james.errington@protonmail.com>
2021-12-21 21:01:31 +0000

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    jquery: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  ignorePatterns: "js/lib/**/*",
+};
