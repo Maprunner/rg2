a87726e5661c02902a4b1793e694b4e613e242d5
Simon Errington <simon@maprunner.co.uk>
2022-03-04 17:12:59 +0000

    Release 1.8.0
    Includes migration to a supported grunt-postcss package.

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@
     DIM: 0.75,
     FULL_INTENSITY: 1.0,
      // version gets set automatically by grunt file during build process
-    RG2VERSION: '1.7.0',
+    RG2VERSION: '1.8.0',
     TIME_NOT_FOUND: 9999,
     // values for evt.which
     RIGHT_CLICK: 3,
